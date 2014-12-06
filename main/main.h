94efe4cad494745c8281a5ed40edb17c2d682c3e
Andres <anfechi1790@gmail.com>
2014-12-06 15:11:19 -0500

    En construccion

--- main/main.h
+++ main/main.h
@@ -1,10 +1,10 @@
 //testing
+
 #include <iostream>
-#include "cTIMER.h"
-#include "cSoft_PWM.h"
-#include "cPOLL_GPIO.h"
-#include "cGPIO.h"
-#include "Gains_LQT_PID2.h"
-#include "PID2.h"
+#include <thread>
+#include <semaphore.h>
+
+extern atomic<int> atom_uk;
+extern sem_t sinc;
 
 using namespace std;
